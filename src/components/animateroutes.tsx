5838a23fb84e0624fd1e541537895fa1cd27ea69
Vitor Ramires <vmr.rs@hotmail.com>
2025-12-29 19:11:32 -0300

    fix: minor fix

--- src/components/animateroutes.tsx
+++ src/components/animateroutes.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from "react-router-dom";
 import { Experiences } from "./experiencias/experiecias";
 import { HomePage } from "./homePage/HomePage";
 import { Project } from "./projetos/projeto";
-import { Services } from "./serviços";
 import { Technologies } from "./tecnologias";
 
 export function AnimateRoutes() {
@@ -10,7 +9,6 @@ export function AnimateRoutes() {
     <>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/services" element={<Services />} />
         <Route path="/experience" element={<Experiences />} />
         <Route path="/project" element={<Project />} />
         <Route path="/technologies" element={<Technologies />} />
