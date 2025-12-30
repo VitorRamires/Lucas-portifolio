2455983dc48049f13916b26bfe4e3f5c8c92aa9e
Vitor Ramires <vmr.rs@hotmail.com>
2025-12-30 10:40:31 -0300

    new slide homePage

--- src/components/homePage/clients.tsx
+++ src/components/homePage/clients.tsx
@@ -1,5 +1,7 @@
 import { useTranslation } from "react-i18next";
 import { projectsInfos } from "../../utils/projects-infos";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 export function Clientes() {
   const { t } = useTranslation();
@@ -7,14 +9,28 @@ export function Clientes() {
     <div className="client-wrapper center">
       <h2>{t("cases")}</h2>
       <div className="clients">
-        {projectsInfos.map((client, index) => {
-          return (
-            <div className="client-item" key={index}>
-              <img src={client.logo} />
-              <p>{client.projeto}</p>
-            </div>
-          );
-        })}
+        <Swiper
+          modules={[Autoplay]}
+          slidesPerView={2}
+          spaceBetween={50}
+          loop={true}
+          centeredSlides={false}
+          parallax={true}
+          autoplay={{ delay: 3000 }}
+          speed={3000}
+          className="mySwiper"
+        >
+          {projectsInfos.map((client, index) => {
+            return (
+              <SwiperSlide>
+                <div className="client-item" key={index}>
+                  <img src={client.logo} />
+                  <p>{client.projeto}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
